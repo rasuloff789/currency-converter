ca881b7fc89c5539c3327bec9fce793814c64021
musmon <usmon.masudjonoff@gmail.com>
2020-10-29 19:45:03 +0500

    Finished currency conveter

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
-alert(`Assalomu alaykum, Karrupsiyaga qarshi kurashish agentligimizga xush kelibsiz brat))`)
+var elCounterForm = document.querySelector('.counter-form');
+var elShowResult = elCounterForm.querySelector('.show-result');
+var elResultInfo = elCounterForm.querySelector('.result');
 
-var elShowResult = document.querySelector('.show-result');
-var elResultInfo = document.querySelector('.result');
-
-elShowResult.addEventListener('click', function (event) {
+elCounterForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  var elCurrency = document.querySelector('.currency-select').value;
+  var elCurrency = elCounterForm.querySelector('.currency-select').value;
   elCurrency = parseFloat(elCurrency, 10);
-  var elCounterInput = document.querySelector('.counter-input').value;
-  elCounterInput = parseFloat(elCounterInput, 10);
+  var elCounterInput = elCounterForm.querySelector('.counter-input').value;
+  var elCounterSum  = parseFloat(elCounterInput, 10);
 
-  var counter = elCounterInput * elCurrency;
+  var counter = (elCounterInput * elCurrency).toFixed(2);
 
-  if (elCounterInput > 0) {
+  if (elCounterSum > 0) {
+    elResultInfo.classList.add('alert');
+    elResultInfo.classList.add('alert-success');
     elResultInfo.textContent = `${counter} so'm`;
-  } else {
-    elResultInfo.textContent = `Karrupsiyaga qarshi kurashish agentligimiz brat ))`
+    elResultInfo.classList.remove('alert');
+    elResultInfo.classList.remove('alert-danger');
+    elResultInfo.classList.remove('bg-dark');
+    elResultInfo.classList.remove('text-white');
+    document.body.style.backgroundImage = "url('../img/dollars.png')";
+    return;
   }
 
-})
+  if (elCounterSum < 0) {
+    elResultInfo.textContent = `Iltimos 0 dan katta son kiriting!`;
+    elResultInfo.classList.add('alert');
+    elResultInfo.classList.add('alert-danger');
+    elResultInfo.classList.remove('bg-dark');
+    elResultInfo.classList.remove('text-white');
+    return;
+  }
+
+  if (isNaN(elCounterSum)) {
+    elResultInfo.textContent = `Summa kiritilmagan, iltmos kiriting!`;
+    elResultInfo.classList.add('alert');
+    elResultInfo.classList.add('alert-danger');
+    elResultInfo.classList.remove('bg-dark');
+    elResultInfo.classList.remove('text-white');
+    return;
+  }
+});
